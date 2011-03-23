c53cd6215c301df6310f7e547b18e031bf138cdb
François Benaiteau <francois.benaiteau@gmail.com>
2011-03-23 23:39:16 +0100

    adding template for rails 2

---
+++ base_template.rb
@@ -0,0 +1,84 @@
+# base_template.rb
+load_template "/Users/netbe/code/bundler_template.rb"
+
+project_dir = root.split('/').last
+
+# Delete unnecessary files
+run "rm README"
+run "rm doc/README_FOR_APP"
+run "rm public/index.html"
+run "rm public/favicon.ico"
+run "rm public/robots.txt"
+run "rm -f public/javascripts/*"
+run "rm db/*.sqlite3"
+
+# Download JQuery
+run "curl -L http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js > public/javascripts/jquery.js"
+
+git :init
+
+file ".gitignore", <<-END
+.DS_Store
+log/*.log
+tmp/**/*
+config/database.yml
+db/*.sqlite3
+END
+run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+
+
+file 'Gemfile', <<-GEMFILE
+source :gemcutter
+gem 'rails', '2.3.10', :require => nil
+# Switch to the appropriate gem for your database
+gem 'mysql'
+gem 'mongrel'
+gem 'haml'
+gem "haml-rails", ">= 0.3.4"
+
+
+GEMFILE
+
+# ===============================================================================
+# = CONFIG FILES                                                                =
+# ===============================================================================
+
+file 'config/database.yml',
+%{development:
+  adapter: mysql
+  encoding: utf8
+  database: #{project_dir}
+  user: root
+  password: root
+  pool: 5
+  timeout: 5000
+
+production:
+  adapter: mysql
+  encoding: utf8
+  database: #{project_dir}_production
+  username: #{project_dir}
+  password: 
+
+test: &TEST
+  adapter: mysql
+  encoding: utf8
+  database: #{project_dir}_test
+  user: root
+  password: root
+  pool: 5
+  timeout: 5000
+
+}
+
+run "cp config/database.yml config/.database.yml"
+
+git :add => ".", :commit => "-m 'initial commit'"
+
+
+generate :controller, "welcome index"
+route "map.root :controller => 'welcome'"
+git :rm => "public/index.html"
+
+git :add => ".", :commit => "-m 'adding welcome controller'"
---
+++ bundler_template.rb
@@ -0,0 +1,49 @@
+file 'config/preinitializer.rb', <<-PRE
+# Use bundled gems
+begin
+  require "rubygems"
+  require "bundler"
+rescue LoadError
+  raise "Could not load the bundler gem. Install it with `gem install bundler`."
+end
+
+if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.24")
+  raise RuntimeError, "Your bundler version is too old for Rails 2.3." +
+   "Run `gem install bundler` to upgrade."
+end
+
+begin
+  # Set up load paths for all bundled gems
+  ENV["BUNDLE_GEMFILE"] = File.expand_path("../../Gemfile", __FILE__)
+  Bundler.setup
+rescue Bundler::GemNotFound
+  raise RuntimeError, "Bundler couldn't find some gems." +
+    "Did you run `bundle install`?"
+end
+PRE
+
+gsub_file 'config/boot.rb', "# All that for this:\nRails.boot!" do |match|
+  <<-BOOT
+# Use bundler
+class Rails::Boot
+  def run
+    load_initializer
+    extend_environment
+    Rails::Initializer.run(:set_load_path)
+  end
+
+  def extend_environment
+    Rails::Initializer.class_eval do
+      old_load = instance_method(:load_environment)
+      define_method(:load_environment) do
+        Bundler.require :default, Rails.env
+        old_load.bind(self).call
+      end
+    end
+  end
+end
+# end bundler support
+ 
+#{match}
+BOOT
+end
